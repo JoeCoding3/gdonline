16e29ad4542917b40ca441bc5b6461f65eec838c
JoeCoding3 <tiredredstoner@gmail.com>
2024-07-18 11:59:40 -0700

    modified:   assets/version/version.js

--- assets/version/version.js
+++ assets/version/version.js
@@ -1,13 +1,17 @@
 let versionNewer = false
 let versionOlder = false
+let versionChecked = false
 registerConsts({
     githubVersionEndpoint: "https://raw.githubusercontent.com/JoeCoding3/versions/main/gdonline.txt",
     gameVersion: 1
 })
 async function getVersion () {
     let resp = await fetch(githubVersionEndpoint)
-    let text = await resp.text()
-    let githubVersion = +text
-    versionNewer = gameVersion > githubVersion
-    versionOlder = gameVersion < githubVersion
+    if (resp.ok) {
+        let text = await resp.text()
+        let githubVersion = +text
+        versionNewer = gameVersion > githubVersion
+        versionOlder = gameVersion < githubVersion
+        versionChecked = true
+    }
 }
